02bafbfaeb3f95b0c6a55a1725bbef1b6928e4bb
Fiori Caka <fioricaka228@gmail.com>
2024-07-27 11:55:49 +0200

    added styling to the cards of the home page

--- js/delete.js
+++ js/delete.js
@@ -3,6 +3,17 @@ document.getElementById('delete-product-btn').addEventListener('click', function
     const checkedBoxes = document.querySelectorAll('.delete-checkbox:checked');
     const ids = Array.from(checkedBoxes).map(box => box.getAttribute('data-id'));
 
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function() {
+            const productDiv = this.parentElement;
+            if (this.checked) {
+                productDiv.classList.add('product-shadow');
+            } else {
+                productDiv.classList.remove('product-shadow');
+            }
+        });
+    });
+
     if (ids.length > 0) {
         fetch('delete-products.php', {
             method: 'POST',
@@ -19,4 +30,6 @@ document.getElementById('delete-product-btn').addEventListener('click', function
               }
           });
     }
+
+
 });
